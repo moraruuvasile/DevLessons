ce9e2a30bcf1fb3cd43026e77006bb4bbd612529
Vasile <vaseoa@yahoo.com>
2020-06-12 20:36:38 +0300

    layouts for portrait + land

--- app/src/main/java/com/example/devlessons/MainActivity.kt
+++ app/src/main/java/com/example/devlessons/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.devlessons
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		val bimap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+		val rounded = RoundedBitmapDrawableFactory.create(resources, bimap)
+//		rounded.cornerRadius = 15f
+		rounded.isCircular = true
+		logo.setImageDrawable(rounded)
 	}
 }
